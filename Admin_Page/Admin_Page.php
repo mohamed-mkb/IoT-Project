923d59562774a49566b2e5318e88401ca0c9b2c9
Mohamed-Amine Makboul <mohamed.mkb2005@gmail.com>
2024-04-15 19:17:02 +0200

    Ajout de navBarre sur interface user

--- Admin_Page/Admin_Page.php
+++ Admin_Page/Admin_Page.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="60">
     <title>Admin-Page</title>
     <link rel="stylesheet" href="Admin_Style.css">
 </head>
--- PHP/envoyer.php
+++ PHP/envoyer.php
@@ -4,8 +4,8 @@
 
 // Insérer les valeur de l'arduino vers la bdd
 
-    $temp = $_GET['temperature'];
-    $hum = $_GET['humidite'];
+    $hum = $_GET['temperature'];
+    $temp = $_GET['humidite'];
 
     $sql = "INSERT INTO valeur (temperature, humidite)
             VALUES ($temp, $hum)";
